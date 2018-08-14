7109088006f95ebbb52f0b8db33ed025e1647e84
Jordan <jordanonuma@outlook.com>
2018-08-13 18:52:02 -1000

    restarted and inserted uPort credentials

--- uPort Run/src/util/connectors.js
+++ uPort Run/src/util/connectors.js
@@ -1,4 +1,8 @@
-import { Connect } from 'uport-connect'
+import { Connect, SimpleSigner } from 'uport-connect'
 
-export let uport = new Connect('TruffleBox')
+export let uport = new Connect('React Uport Truffle Boilerplate',{
+  clientId: '2ojRm8K6yhos1318emGGAnaYH4UCqz4qYWg',
+  network: 'rinkeby',
+  signer: SimpleSigner('SIGNING KEY')
+})
 export const web3 = uport.getWeb3()
